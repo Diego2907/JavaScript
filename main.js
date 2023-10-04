33bb710bba22e141a2590beeb9a31c273024c6c1
Diego2907 <dverduzco10@ucol.mx>
2023-10-03 21:42:27 -0600

    Tarea

---
+++ main.js
@@ -0,0 +1,35 @@
+// declaracion de variables
+// let contador = 5; 
+// contador = 8
+// const constante = 4; 
+// const
+// let
+
+
+// alert('hola desde una alerta desde javascript')
+// console.log('esto es javascript desde el HTML');
+// console.log(constante);
+// console.log(numero);
+
+
+let nombre = 'Diego';
+let apellido = 'Verduzco';
+console.log('Mi nombre es:'+' '+nombre+' '+apellido);
+
+document.write('Mi nombre es:'+' '+nombre+' '+apellido )
+
+let seleccion = document.querySelector('#seccion1');
+seleccion.innerHTML = `<h3>MiNombreEs:$nombre </h3>`;
+
+
+
+
+
+
+
+
+
+
+
+
+
